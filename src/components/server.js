7f89b8aea5e5f1f2affbb4e9006cf5f61cbf93da
hashi-kavi <kavindyaranaweera14@gmail.com>
2025-03-01 01:43:04 +0530

    Add new assets and server file

---
+++ src/components/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
